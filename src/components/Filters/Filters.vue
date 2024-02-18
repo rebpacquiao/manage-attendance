9b4fc0ad796aeef5123e66ff20e90dfcb24a8a91
rebpacquiao <reb.pacquiao@gmail.com>
2024-02-18 10:40:21 +0800

    Update set up structure of filters

--- src/components/Filters/Filters.vue
+++ src/components/Filters/Filters.vue
@@ -16,75 +16,68 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { NSelect, NSpace } from "naive-ui";
 import { companyData } from "@/data/companyData.js";
 import { deparmentData } from "@/data/departmentData.js";
 import { locationsData } from "@/data/locationData.js";
 import { Employee } from "@/data/employee.js";
-
 export default defineComponent({
   name: "FilterComponent",
   components: {
     NSelect,
     NSpace,
   },
-  data() {
-    return {
-      filters: [
-        {
-          label: "Company",
-          model: "selectedCompany",
-          placeholder: "Select company",
-          options: companyData,
-          multiple: false,
-        },
-        {
-          label: "Department",
-          model: "selectedDeparment",
-          placeholder: "Select department",
-          options: deparmentData,
-          multiple: false,
-        },
-        {
-          label: "Location",
-          model: "selectedLocation",
-          placeholder: "Select location",
-          options: locationsData,
-          multiple: false,
-        },
-        {
-          label: "Employee",
-          model: "selectedEmployee",
-          placeholder: "Select employee",
-          multiple: true,
-          options: Employee.map((employee) => ({
-            label: `${employee.firstName} ${employee.lastName}`,
-            value: `${employee.firstName} ${employee.lastName}`,
-          })),
-        },
-      ],
-    };
-  },
-  computed: {
-    employeeOptions() {
-      return Employee.map((employee) => ({
+  setup() {
+    const filters = ref([
+      {
+        label: "Company",
+        model: "selectedCompany",
+        placeholder: "Select company",
+        options: companyData,
+        multiple: false,
+      },
+      {
+        label: "Department",
+        model: "selectedDeparment",
+        placeholder: "Select department",
+        options: deparmentData,
+        multiple: false,
+      },
+      {
+        label: "Location",
+        model: "selectedLocation",
+        placeholder: "Select location",
+        options: locationsData,
+        multiple: false,
+      },
+      {
+        label: "Employee",
+        model: "selectedEmployee",
+        placeholder: "Select employee",
+        multiple: true,
+        options: Employee.map((employee) => ({
+          label: `${employee.firstName} ${employee.lastName}`,
+          value: `${employee.firstName} ${employee.lastName}`,
+        })),
+      },
+    ]);
+
+    const employeeOptions = ref(
+      Employee.map((employee) => ({
         label: `${employee.firstName} ${employee.lastName}`,
         value: `${employee.firstName} ${employee.lastName}`,
-      }));
-    },
-  },
-  methods: {
-    handleSelectChange(selectedValue) {
+      }))
+    );
+
+    const handleSelectChange = (selectedValue) => {
       console.log(`Selected value for `, selectedValue);
-    },
-  },
-  setup() {
+    };
+
     return {
-      companyData,
-      deparmentData,
-      locationsData,
-      Employee,
+      filters,
+      employeeOptions,
+      handleSelectChange,
     };
   },
 });
