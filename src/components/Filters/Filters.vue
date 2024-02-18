92717009ec7297535a7c171bd91ddbe2308d87ae
rebpacquiao <reb.pacquiao@gmail.com>
2024-02-18 09:30:15 +0800

    Fixed issue on Placeholder

--- src/components/Filters/Filters.vue
+++ src/components/Filters/Filters.vue
@@ -1,25 +1,20 @@
 <template>
-  <NFlex vertical>
-    <div
-      v-for="filterItem in filterItems"
-      :key="filterItem.label"
-      :class="filterItem.class"
-    >
-      <label>{{ filterItem.label }}</label>
+  <NSpace vertical>
+    <div v-for="(filter, index) in filters" :key="index" class="filter-items">
+      <label>{{ filter.label }}</label>
       <NSelect
-        v-model:value="filterItem.model"
+        :v-model="filter.model"
+        :placeholder="filter.placeholder"
+        :options="filter.options"
         size="large"
-        :placeholder="filterItem.placeholder"
-        :options="filterItem.options"
       />
     </div>
-  </NFlex>
-  <div></div>
+  </NSpace>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { NSelect, NFlex } from "naive-ui";
+import { NSelect, NSpace } from "naive-ui";
 import { companyData } from "@/data/companyData.js";
 import { deparmentData } from "@/data/departmentData.js";
 import { locationsData } from "@/data/locationData.js";
@@ -29,40 +24,36 @@ export default defineComponent({
   name: "FilterComponent",
   components: {
     NSelect,
-    NFlex,
+    NSpace,
   },
   data() {
     return {
-      filterItems: [
+      filters: [
         {
           label: "Company",
-          class: "filter-items",
           model: "selectedCompany",
           placeholder: "Select company",
           options: companyData,
         },
         {
           label: "Department",
-          class: "filter-items",
-          model: "selectedDepartment",
+          model: "selectedDeparment",
           placeholder: "Select department",
           options: deparmentData,
         },
         {
           label: "Location",
-          class: "filter-items",
           model: "selectedLocation",
           placeholder: "Select location",
           options: locationsData,
         },
         {
           label: "Employee",
-          class: "filter-items",
           model: "selectedEmployee",
-          placeholder: "Select Employee",
+          placeholder: "Select employee",
           options: Employee.map((employee) => ({
             label: `${employee.firstName} ${employee.lastName}`,
-            value: employee,
+            value: `${employee.firstName} ${employee.lastName}`,
           })),
         },
       ],
