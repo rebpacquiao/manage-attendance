1a2400f05f9fecbcbcf0f0505e2983eb8c9c2664
rebpacquiao <reb.pacquiao@gmail.com>
2024-02-19 21:01:03 +0800

    upload dist

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/manage-attendance-app/" : "/",
   transpileDependencies: true,
   css: {
     loaderOptions: {
